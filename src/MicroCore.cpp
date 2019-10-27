3344dc02a327bfa34474d68b741574f65ca2eac6
Learner <linhdqtg@gmail.com>
2019-10-28 00:22:19 +0700

    Update explorer

--- src/MicroCore.cpp
+++ src/MicroCore.cpp
@@ -35,24 +35,18 @@ MicroCore::MicroCore():
  * Initialize m_blockchain_storage with the BlockchainLMDB object.
  */
 bool
-MicroCore::init(const string& _blockchain_path, network_type nt)
+MicroCore::init(const string& blockchain_path_, network_type nt)
 {
-    int db_flags = 0;
 
-    blockchain_path = _blockchain_path;
-
-    nettype = nt;
-
-    db_flags |= MDB_RDONLY;
-    db_flags |= MDB_NOLOCK;
-
-    BlockchainDB* db = nullptr;
-    db = new BlockchainLMDB();
+    blockchain_path = blockchain_path_;
+    nettype         = nt;
+    int db_flags    = DBF_RDONLY;
+    BlockchainDB* db = new BlockchainLMDB();
 
     try
     {
         // try opening lmdb database files
-        db->open(blockchain_path, db_flags);
+        db->open(blockchain_path, nettype,  db_flags);
     }
     catch (const std::exception& e)
     {
